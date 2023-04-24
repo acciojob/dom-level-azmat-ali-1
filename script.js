9ea8d9f71e474afd4780f6f58bdf55aefa8227bd
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-25 03:48:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const f = document.querySelectorAll("ul li");
+for (let index = 0; index < f.length; index++) {
+  const element = f[index];
+  if(element.id=="level"){
+    alert("The level of the element is: "+ element.innerText);
+  }
+  
+} 
